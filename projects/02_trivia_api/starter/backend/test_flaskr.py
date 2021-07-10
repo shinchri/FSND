0de52f0d3eeddeafd995dff0a2ba16906f82b1f3
Chris Shin <chrisshin@Kellys-iMac.hitronhub.home>
2021-07-10 14:06:06 -0500

    comit for review

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -5,10 +5,40 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+categories = [
+    {"id": 1, "type": "science"},
+    {"id": 2, "type": "art"},
+    {"id": 3, "type": "geography"},
+    {"id": 4, "type": "history"},
+    {"id": 5, "type": "entertainment"},
+    {"id": 6, "type": "sports"}
+]
 
+questions = [
+    {"question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?", "answer": "Maya Angelou", "difficulty": 2, "category": 4},
+    {"question": "What boxer's original name is Cassius Clay?", "answer": "Muhammad Ali", "difficulty": 1, "category": 4},
+    {"question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?", "answer": "Apollo 13", "difficulty": 4, "category": 5},
+    {"question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?", "answer": "Tom Cruise", "difficulty": 4, "category": 5},
+    {"question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?", "answer": "Edward Scissorhands", "difficulty": 3, "category": 5},
+    {"question": "Which is the only team to play in every soccer World Cup tournament?", "answer": "Brazil", "difficulty": 3, "category": 6},
+    {"question": "Which country won the first ever soccer World Cup in 1930?", "answer": "Uruguay", "difficulty": 4, "category": 6},
+    {"question": "Who invented Peanut Butter?", "answer": "George Washington Carver", "difficulty": 2, "category": 4},
+    {"question": "What is the largest lake in Africa?", "answer": "Lake Victoria", "difficulty": 2, "category": 3},
+    {"question": "In which royal palace would you find the Hall of Mirrors?", "answer": "", "difficulty": 3, "category": 3},
+    {"question": "The Taj Mahal is located in which Indian city?", "answer": "", "difficulty": 2, "category": 3},
+    {"question": "Which Dutch graphic artist–initials M C was a creator of optical illusions?", "answer": "Escher", "difficulty": 1, "category": 2},
+    {"question": "La Giaconda is better known as what?", "answer": "Mona Lisa", "difficulty": 3, "category": 2},
+    {"question": "How many paintings did Van Gogh sell in his lifetime?", "answer": "One", "difficulty": 4, "category": 2},
+    {"question": "Which American artist was a pioneer of Abstract Expressionism, and a leading exponent of action painting?", "answer": "Jackson Pollock", "difficulty": 2, "category": 2},
+    {"question": "What is the heaviest organ in the human body?", "answer": "The Liver", "difficulty": 4, "category": 1},
+    {"question": "Who discovered penicillin?", "answer": "Alexander Fleming", "difficulty": 3, "category": 1},
+    {"question": "Hematology is a branch of medicine involving the study of what?", "answer": "Blood", "difficulty": 4, "category": 1},
+    {"question": "Which dung beetle was worshipped by the ancient Egyptians?", "answer": "Scarab", "difficulty": 4, "category": 4}
+]
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
+
 
     def setUp(self):
         """Define test variables and initialize app."""
@@ -35,11 +65,26 @@ class TriviaTestCase(unittest.TestCase):
             # create all tables
             self.db.create_all()
 
+            # for category in categories:
+            #     obj = Category(type=category['type'])
+            #     self.db.session.add(obj)
+                
+            #     self.db.session.commit()
+
+            # for question in questions:
+            #     obj = Question(question=question['question'], answer=question['answer'], category=question['category'], difficulty=question['difficulty'])
+            #     self.db.session.add(obj)
+            
+            #     self.db.session.commit()
+
+            #     self.db.session.close()
         
     
     def tearDown(self):
         """Executed after reach test"""
-        pass
+        # print("s;dfnk;sldkfn")
+        # with self.app.app_context():
+        #     self.db.drop_all(app=self.app)
 
     
 
@@ -81,7 +126,7 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['questions'])
-        self.assertEqual(data['total_questions'], 2)
+        self.assertEqual(data['total_questions'], 3)
         self.assertEqual(data['current_category'], 1)
 
     def test_422_for_invalid_category_id(self):
@@ -118,14 +163,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['current_category'], None)
 
     def test_delete_questions(self):
-        res = self.client().delete('/questions/27') # use different id everytime
+        res = self.client().delete('/questions/9') # use different id everytime
         data = json.loads(res.data)
 
-        question = Question.query.filter(Question.id==27).one_or_none()
+        question = Question.query.filter(Question.id==9).one_or_none()
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deleted'], 27)
+        self.assertEqual(data['deleted'], 9)
         self.assertEqual(question, None)
 
     def test_422_delete_none_existing_question(self):
