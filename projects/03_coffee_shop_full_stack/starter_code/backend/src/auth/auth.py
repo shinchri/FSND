6c01a889cff396ef090f8d68196407fb9c9006f3
Chris Shin <chrisshin@Kellys-iMac.hitronhub.home>
2021-07-12 15:14:07 -0500

    set up Auth0 and fixed some error in model

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-oops.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'drink'
 
 ## AuthError Exception
 '''
