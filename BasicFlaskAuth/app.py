9681759f4aeed0f18620a3979862fc36ea976c6f
Chris Shin <chrisshin@Kellys-iMac.hitronhub.home>
2021-07-11 19:54:14 -0500

    changed requirement and practice app for Auth0

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = "dev-oops.us.auth0.com"
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = "image"
 
 
 class AuthError(Exception):
@@ -52,8 +52,14 @@ def get_token_auth_header():
 
 
 def verify_decode_jwt(token):
-    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
-    jwks = json.loads(jsonurl.read())
+    #jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    #jwks = json.loads(jsonurl.read())
+    myurl = 'https://%s/.well-known/jwks.json' % (AUTH0_DOMAIN)
+    jsonurl = urlopen(myurl)
+    content = jsonurl.read().decode(jsonurl.headers.get_content_charset())
+    jwks = json.loads(content)
+    
+    
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
     if 'kid' not in unverified_header:
@@ -104,21 +110,40 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        abort(400)
+    
+    if permission not in payload['permissions']:
+        abort(403)
+    
+    return True
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+def requires_auth(permission='.'):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                token = get_token_auth_header()
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
 
-    return wrapper
+            check_permissions(permission, payload)
+            
+            return f(payload, *args, **kwargs)
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
+        return wrapper
+    return requires_auth_decorator
+
+# @app.route('/headers')
+# @requires_auth
+# def headers(payload):
+#     print(payload)
+#     return 'Access Granted'
+
+@app.route('/image')
+@requires_auth('get:images')
+def images(payload):
     print(payload)
-    return 'Access Granted'
+    return 'not implemented'
